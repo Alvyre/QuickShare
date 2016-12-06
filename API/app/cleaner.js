bfb28a3861a0d38404cb3196fd8c6c884e8bc71c
Romain <belum76@gmail.com>
2016-12-06 18:26:04 +0900

    fix : cleaner script
    Delete now infos which expirydate is older than now()    not birthdate anymore

--- API/app/cleaner.js
+++ API/app/cleaner.js
@@ -7,7 +7,7 @@ module.exports = {
 	deleteOldInfo() {
 		var now = moment().utc().format();
 
-		Info.remove({birthdate: {$lt: moment().utc().format()} }, function(err) {
+		Info.remove({expirydate: {$lt: moment().utc().format()} }, function(err) {
 			if(err) {
 				console.log('Error when trying to delete old infos');
 				return;
